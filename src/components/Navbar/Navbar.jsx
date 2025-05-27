08955e22d42e4e2e61195672762626215cfeaec0
surya87567 <singhsuryapratap058@gmail.com>
2025-05-27 15:59:06 +0530

    added responsiveness to the webpage

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
-import './navbar.css'
+
+import { useContext } from 'react'
+import { CoinContext } from '../../context/CoinContext'
+import './Navbar.css'
 import logo from '../../assets/logo.png'
 import arrow_icon from '../../assets/arrow_icon.png'
 
-const navbar = () => {
+let Navbar = () => {
+
+  const { setCurrency } = useContext(CoinContext);
+  const currencyHandler = (event)=>{
+     switch (event.target.value){
+      case "usd":
+        setCurrency({
+          name: "USD",
+          symbol: "$"
+        });
+        break;
+      case "eur":
+        setCurrency({
+          name: "EUR",
+          symbol: "€"
+        });
+        break;
+      case "inr":
+        setCurrency({
+          name: "INR",
+          symbol: "₹"
+        });
+        break;
+      default:
+        setCurrency({
+          name: "USD",
+          symbol: "$"
+        });
+     }
+  }
+  
   return (
     <div className='navbar'>
       <img src={logo} alt="" className='logo' />
@@ -15,7 +47,7 @@ const navbar = () => {
         <li>Blog</li>
       </ul>
       <div className="nav-right">
-        <select>
+        <select onChange={currencyHandler}>
           <option value="usd">USD</option>
           <option value="eur">EUR</option>
           <option value="inr">INR</option>
@@ -27,4 +59,4 @@ const navbar = () => {
   )
 }
 
-export default navbar
+export default Navbar
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,67 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { CoinContext } from '../../context/CoinContext'
 import './Home.css'
 
 const Home = () => {
+
+  const {allCoin,currency}= useContext(CoinContext);
+  const [displayCoin, setDisplayCoin] = useState([]);
+
+  const [input, seInput] = useState("");
+
+  const inputHandler = (event) => {
+    seInput(event.target.value);
+  }
+
+  const searchHandler = async(event) => {
+    event.preventDefault();
+    const coins=await allCoin.filter((item) => {
+      return item.name.toLowerCase().includes(input.toLocaleLowerCase())
+    })
+
+    setDisplayCoin(coins)
+
+  }
+
+  useEffect(() => {
+    setDisplayCoin(allCoin);
+  },[allCoin])
+
   return (
     <div className='home'>
       <div className="hero">
-        <h1>Largest <br/> Crypto Mrketplace</h1>
+        <h1>Largest <br/> Crypto Marketplace</h1>
         <p>Welcome to the world's largest cryptocurrency marketplace. Sign up to explore more about cryptos.</p>
-        <form>
-          <input type="text" placeholder='Search crypto..'/>
+        <form onSubmit={searchHandler}>
+          <input onChange={inputHandler} value={input} type="text" placeholder='Search crypto..' required/>
           <button type='submit'>Search</button>
         </form>
+      </div>
+      <div className="crypto-table">
+        <div className="table-layout">
+          <p>#</p>
+          <p>Coins</p>
+          <p>Price</p>
+          <p style={{textAlign:"center"}}>24H Change</p>
+          <p className='market-cap'>Market Cap</p>
+        </div>
+        {
+          displayCoin.slice(0,10).map((item,index)=>(
+            <div className="table-layout" key={index}>
+               <p>{item.market_cap_rank}</p>
+               <div>
+                <img src={item.image} alt="" />
+                <p>{item.name+ " - " +item.symbol}</p>
+               </div>
+               <p>{currency.symbol} {item.current_price.toLocaleString()}</p>
+            
+               <p className={item.price_change_percentage_24h>0? "green": "red" }>
+                {Math.floor(item.price_change_percentage_24h*100)/100}</p>
+               <p className='market-cap' >{currency.symbol} {item.market_cap.toLocaleString()}</p>
+            </div>
+
+          ))
+        }
       </div>
     </div>
   )
