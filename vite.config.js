5a3a1c35901faa727b361a57ace9480023cf15d1
surya87567 <singhsuryapratap058@gmail.com>
2025-07-25 00:16:01 +0530

    fixed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/react-vite-deploy",
 })
